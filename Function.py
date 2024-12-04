ee0ed763be8e11742ed9d821111e519f1111f8da
Schwaby400 <cschwab9067@gmail.com>
2024-12-03 19:44:49 -0500

    day2update

---
+++ Function.py
@@ -0,0 +1,9 @@
+def add(number1):
+    return number1 + 50
+x = add(2)
+print(x)
+z = add(30)
+print(z)
+a = add(x)
+a = str(a)
+print("the value of a is" + a)
---
+++ Variables.py
@@ -0,0 +1,6 @@
+x = 5
+a = 10
+b = 6
+c = a + b
+d = a + x
+print(d)
